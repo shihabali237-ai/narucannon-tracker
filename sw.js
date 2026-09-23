8fa8c4e226c5c98ba18e2454b5bf8197dbe92ab2
shihabali237-ai <shihabali237@gmail.com>
2026-09-23 18:21:48 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "narucannon-v1";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js", "./icon-192.png", "./icon-512.png"];
+const CACHE = "narucannon-v4";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
